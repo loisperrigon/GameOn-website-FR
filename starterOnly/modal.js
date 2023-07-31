88000de4b77b66e6c165d972f4455f57f912b96f
lois.perrigon@hotmail.fr <lois.perrigon@hotmail.fr>
2023-07-31 18:57:46 +0700

    Ajout de la fonction close (Ferme le formulaire)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalbg = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeModalbg.forEach((close) => close.addEventListener("click", closeLauchModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+
+function closeLauchModal(){
+  modalbg.style.display ="none";
 }
 
 
